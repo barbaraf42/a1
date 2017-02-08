bb3bff41bee34fb74accd863229a1857f5252692
Barbara Finkemeyer <barbara.finkemeyer@gmail.com>
2017-02-08 11:35:28 -0500

    initial commit

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+    <link rel="stylesheet" href="styles/styles.css" />
+
+</head>
+<body>
+
+<section class="nameplate">
+    <img class="headshot" src="images/headshot.jpg" alt="Picture of Barbara">
+    <div class="bio">
+        <h1>Hi, I'm Barbara!</h1>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+    </div>
+</section>
+
+<section class="quote">
+    quote will be here
+</section>
+
+</body>
+</html>
