311a517c09172f62d1ccf07568c1356430a654e0
Barbara Finkemeyer <barbara.finkemeyer@gmail.com>
2017-02-08 19:16:46 -0500

    updated content, added quotes and php logic

--- index.php
+++ index.php
@@ -1,26 +1,30 @@
+<?php require('php-logic/quote.php'); ?>
+
 <!DOCTYPE html>
 <html>
 <head>
 
-<title>Hi, I'm Barbara!</title>
-<link rel="stylesheet" href="styles/styles.css" />
+    <title>Hi, I'm Barbara!</title>
+    <link rel="stylesheet" href="styles/styles.css" />
 
 </head>
 <body>
 
-<section class="nameplate">
-    <img class="headshot" src="images/headshot.jpg" alt="Picture of Barbara">
-    <div class="bio">
-        <h1>Hi, I'm Barbara!</h1>
-        <p>
-            I'm a front-end web developer living in the Boston, Massachusetts, area, trying to get up to speed on the rest of the web development stack. I enjoy a good challenge, but I really appreciate learning things from experienced people even more, so I'm taking this class. My two dogs keep me company while I'm coding, unless there are squirrels outside - then we switch jobs, and I keep them company outside while they chase down those fierce beasts.
-        </p>
-    </div>
-</section>
+    <section class="nameplate">
+        <img class="headshot" src="images/headshot.jpg" alt="Picture of Barbara">
+        <div class="bio">
+            <h1>Hi, I'm Barbara!</h1>
+            <p>
+                I'm a front-end web developer living near Boston, Massachusetts, trying to get up to speed on the rest of the web development stack. I enjoy a good challenge, but I really appreciate learning things from experienced people even more, so I'm taking this class. My two dogs keep me company while I'm coding, unless there are squirrels outside - then we switch jobs, and I keep them company outside while they chase down those fierce beasts.
+            </p>
+        </div>
+    </section>
 
-<section class="quote">
-    quote will be here
-</section>
+    <div class="quote">
+        "<?php echo $quote; ?>"
+        <br />
+        - <?php echo $source; ?>
+    </div>
 
 </body>
 </html>
